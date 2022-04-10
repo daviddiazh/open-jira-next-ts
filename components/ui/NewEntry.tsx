80cbceec57cefa7a8bf9b9b5d4ed9cf130f30904
daviddiazh <daviddiazlds1221@gmail.com>
2022-04-10 14:06:50 -0500

    Se hacen validaciones para errores en el formulario

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,43 +1,73 @@
+import { ChangeEvent, useState } from 'react';
 import { Button, TextField } from '@mui/material'
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import AddIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import { Box } from '@mui/system';
 
 export const NewEntry = () => {
+
+    const [isAdding, setIsAdding] = useState(false);
+
+    const [inputValue, setInputValue] = useState('');
+    const [touched, setTouched] = useState(false);
+
+    const onTextFieldChanged = ( event: ChangeEvent<HTMLInputElement> ) => {
+        setInputValue( event.target.value );
+    }
+
+    const onSave = () => {
+        if( inputValue.length <= 1 ) return;
+
+        console.log(inputValue)
+    }
+
   return (
     <Box sx={{ marginBottom: 2, paddingX: 2 }}>
-        <Button
-            startIcon={ <AddIcon /> }
-            fullWidth
-            variant='outlined'
-        >
-            Agregar Tarea
-        </Button>
 
-        <TextField 
-            fullWidth
-            sx={{ marginTop: 2, marginBottom: 1}}
-            placeholder='Nueva Entrada'
-            autoFocus
-            multiline
-            label='Nueva Entrada'
-            helperText='Ingrese un valor'
-        />
-
-        <Box display='flex' justifyContent='space-between'>
-            <Button 
-                variant='text'
-            >
-                Cancelar
-            </Button>
-            <Button 
-                variant='outlined'
-                color='secondary'
-                endIcon={ <SaveOutlinedIcon/> }
-            >
-                Guardar
-            </Button>
-        </Box>
+        {
+            isAdding ? (
+                <>
+                    <TextField 
+                        fullWidth
+                        sx={{ marginTop: 2, marginBottom: 1}}
+                        //placeholder='Nueva Entrada'
+                        autoFocus
+                        multiline
+                        label='Nueva Entrada'
+                        helperText={ inputValue.length <= 1 && touched && 'Ingrese un valor' }
+                        error={ inputValue.length <= 1 && touched }
+                        value={ inputValue }
+                        onChange={ onTextFieldChanged }
+                        onBlur={ () => setTouched( true ) }
+                    />
+                    <Box display='flex' justifyContent='space-between'>
+                        <Button 
+                            variant='text'
+                            onClick={() => setIsAdding(false)}
+                        >
+                            Cancelar
+                        </Button>
+                        <Button 
+                            variant='outlined'
+                            color='secondary'
+                            endIcon={ <SaveOutlinedIcon/> }
+                            onClick={ onSave }
+                        >
+                            Guardar
+                        </Button>
+                    </Box>
+                </>
+            ) : (
+                <Button
+                    startIcon={ <AddIcon /> }
+                    fullWidth
+                    variant='outlined'
+                    onClick={() => setIsAdding(true)}
+                >
+                    Agregar Tarea
+                </Button>
+            )
+        }
     </Box>
   )
 }
