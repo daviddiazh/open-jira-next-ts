1fcba1dc2cecac26e228ed8b30dac51158c61db3
daviddiazh <daviddiazlds1221@gmail.com>
2022-04-10 01:30:12 -0500

    Se crea componente para el form de nueva tarea

---
+++ components/ui/NewEntry.tsx
@@ -0,0 +1,43 @@
+import { Button, TextField } from '@mui/material'
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import AddIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import { Box } from '@mui/system';
+
+export const NewEntry = () => {
+  return (
+    <Box sx={{ marginBottom: 2, paddingX: 2 }}>
+        <Button
+            startIcon={ <AddIcon /> }
+            fullWidth
+            variant='outlined'
+        >
+            Agregar Tarea
+        </Button>
+
+        <TextField 
+            fullWidth
+            sx={{ marginTop: 2, marginBottom: 1}}
+            placeholder='Nueva Entrada'
+            autoFocus
+            multiline
+            label='Nueva Entrada'
+            helperText='Ingrese un valor'
+        />
+
+        <Box display='flex' justifyContent='space-between'>
+            <Button 
+                variant='text'
+            >
+                Cancelar
+            </Button>
+            <Button 
+                variant='outlined'
+                color='secondary'
+                endIcon={ <SaveOutlinedIcon/> }
+            >
+                Guardar
+            </Button>
+        </Box>
+    </Box>
+  )
+}
