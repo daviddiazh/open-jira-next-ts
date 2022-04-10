f4c98f62d3c688a21e31918c5752948b6d8f2617
daviddiazh <daviddiazlds1221@gmail.com>
2022-04-09 21:29:17 -0500

    Se crea la vista principal para visualizar los Work Items

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,34 @@
-import { Typography } from "@mui/material";
 import type { NextPage } from "next";
+import { Card, CardContent, CardHeader, Grid } from "@mui/material";
 import { Layout } from "../components/layouts/";
-import { title } from 'process';
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant="h1" color='primary'>Hola Mundo</Typography>
+    <Layout title='Home App'>
+      <Grid container spacing={ 2 }>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='Pendientes' />
+            <CardContent/>
+              
+            <CardContent/>
+          </Card>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='En Progreso' />
+          </Card>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='Completadas' />
+          </Card>
+        </Grid>
+
+      </Grid>
     </Layout>
   );
 };
