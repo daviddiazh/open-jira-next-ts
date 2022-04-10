0b0144388ed4815aa726435ebc63c669513ddf38
daviddiazh <daviddiazlds1221@gmail.com>
2022-04-09 20:21:34 -0500

    Se aggrega el uiReducer

--- context/ui/uiReducer.ts
+++ context/ui/uiReducer.ts
@@ -1,4 +1,25 @@
+import { UIState } from './UIProvider';
 
-export const uiReducer = () => {
-    
+type UIActionTypes = 
+|   { type: 'Ui - Open sidebar' }
+|   { type: 'Ui - Close sidebar' }
+
+export const uiReducer = ( state: UIState, action: UIActionTypes ): UIState => {
+
+    switch( action.type ){
+        case 'Ui - Open sidebar':
+            return {
+                ...state,
+                sidemenuOpen: true
+            }
+        case 'Ui - Close sidebar':
+            return {
+                ...state,
+                sidemenuOpen: false
+            }
+        default:
+            return state;
+    }
+
 }
+
