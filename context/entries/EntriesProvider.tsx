e039d5afd3e68cd39c1d559df0a12e3be4327ab9
daviddiazh <daviddiazlds1221@gmail.com>
2022-04-17 21:32:54 -0500

    Se llama el endPoint en el FRONT para actualizar

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -32,8 +32,13 @@ export const EntriesProvider: FC = ({ children }) => {
 
     }
 
-    const updateEntry = ( entry: Entry ) => {
-        dispatch({ type: 'Entry - Entry-Update', payload: entry })
+    const updateEntry = async ( { _id, description, status }: Entry ) => {
+        try {
+            const { data } = await entriesApi.put<Entry>(`/entries/${_id}`, { description, status, })
+            dispatch({ type: 'Entry - Entry-Update', payload: data })
+        } catch (error) {
+            console.log({error})
+        }
     }
 
     const refreshEntries = async () => {
